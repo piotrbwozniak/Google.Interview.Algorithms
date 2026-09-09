d1177c3b1d2e628747a5bc9a2f5164d908647584
Piotr Wozniak <piotr.b.wozniak@gmail.com>
2026-09-09 11:17:00 +0200

    Add project files.

---
+++ HashMap/Program.cs
@@ -0,0 +1,26 @@
+﻿int[] nums = { 1, 2, 3, 4, 3 };
+
+bool result = ContainsDuplicate(nums);
+
+Console.WriteLine(result);
+
+Console.ReadKey();
+
+bool ContainsDuplicate(int[] nums)
+{
+
+    var seen = new HashSet<int>();
+
+    foreach(var x in nums)
+    {
+        if(seen.Contains(x))
+        {
+            return true;
+        }
+
+        seen.Add(x);
+    }
+
+    return false;
+}
+
